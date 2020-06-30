a6c3e65ece1dfc8548fa3ce264ee065669d9b8ff
Pietro <iopietro@hotmail.it>
2020-06-30 17:02:21 +0200

    Nothing

--- BetterScp939/Properties/AssemblyInfo.cs
+++ BetterScp939/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // Le informazioni generali relative a un assembly sono controllate dal seguente 
 // set di attributi. Modificare i valori di questi attributi per modificare le informazioni
 // associate a un assembly.
-[assembly: AssemblyTitle("BetterSCP939")]
+[assembly: AssemblyTitle("BetterScp939")]
 [assembly: AssemblyDescription("An upgraded version of the already existing SCP-939.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("iopietro")]
-[assembly: AssemblyProduct("BetterSCP939")]
+[assembly: AssemblyProduct("BetterScp939")]
 [assembly: AssemblyCopyright("Copyright © iopietro 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
 // usando l'asterisco '*' come illustrato di seguito:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.4")]
-[assembly: AssemblyFileVersion("1.0.4")]
+[assembly: AssemblyVersion("1.0.3")]
+[assembly: AssemblyFileVersion("1.0.3")]
