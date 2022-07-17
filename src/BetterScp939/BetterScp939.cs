eec0af588a84fac17442b8834d107f48125cfc3a
Pietro <iopietro@hotmail.it>
2022-07-17 02:14:36 +0200

    Updated RequiredExiledVersion

--- src/BetterScp939/BetterScp939.cs
+++ src/BetterScp939/BetterScp939.cs
@@ -1,4 +1,4 @@
-﻿namespace BetterScp939
+namespace BetterScp939
 {
     using Components;
     using Events;
@@ -11,7 +11,7 @@
         private static readonly BetterScp939 InstanceValue = new BetterScp939();
         private PlayerHandler playerHandler;
 
-        public override Version RequiredExiledVersion { get; } = new Version(5, 0, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(5, 3, 0);
 
         private BetterScp939()
         {
